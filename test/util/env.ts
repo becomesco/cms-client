05e740d94434c3a98199220d3552236223ee1130
Bbanez <banezprod@gmail.com>
2020-10-02 13:22:07 +0200

    Initial development

--- test/util/env.ts
+++ test/util/env.ts
@@ -1,12 +1,12 @@
 export const Env = {
   cmsOrigin: 'http://localhost:1281',
   key: {
-    id: '5f6f0e8975196b5eb72f29b3',
-    secret: '1bbbbd06c06488cc0c23a9184744737f1063a615e30d77756d2555805c032e12',
+    id: '5f76e4af3a337c3256937dde',
+    secret: '576941ad9e21c2fe6e7129e0f971b3dd16e07b8b46fef5795fc101eeb7ca25c2',
   },
   templateId: '5f72c44d2f8aad26c0b2b002',
   entryId: '5f737325c0986246c710dd1d',
   mediaId: '5f6f9531620c66a45b991f97',
-  publicFunctionName: 'my-public-function',
-  functionName: 'test-function',
+  publicFunctionName: 'pub',
+  functionName: 'test',
 };
